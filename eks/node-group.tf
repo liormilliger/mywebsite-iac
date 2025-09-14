f2054bab01dbb5db309c11e2e6bf5f077274904c
Lior Milliger <liormilliger@gmail.com>
2025-09-14 12:14:47 +0300

    launching nodes on public subnets - working configuration

--- eks/node-group.tf
+++ eks/node-group.tf
@@ -40,8 +40,8 @@ resource "aws_eks_node_group" "node-group" {
   node_role_arn   = aws_iam_role.liorm-node-group-role.arn
 
   subnet_ids = [
-    local.private-us-east-1a-id,
-    local.private-us-east-1b-id
+    local.public-us-east-1a-id,
+    local.public-us-east-1b-id
   ]
 
   capacity_type  = var.capacity_type
